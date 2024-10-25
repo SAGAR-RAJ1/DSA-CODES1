b1a3f3f0a15f9950c802792cb7284a004fc70053
SAGAR-RAJ1 <ksanjeev49919+github@gmail.com>
2024-10-25 09:49:41 +0530

    included merge sort

---
+++ Recursion/VIDEO 59 merge sort/merge.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+
+void merge(int arr[],int start,int mid,int end){
+vector<int>temp(end-start+1);
+int left=start;
+int right=mid+1;
+int index=0;
+//sorting and filling
+while (left<=mid && right<=end)
+{
+   if (arr[left]<=arr[right])
+   {
+    temp[index]=arr[left];
+    index++;left++;
+   }
+   else{
+      temp[index]=arr[+right];
+    index++;right++;
+   }
+}
+
+ //left overs
+
+ //if left one left
+
+ while (left<=mid)
+ {
+    temp[index]=arr[left];
+    index++;left++;
+    
+ }
+ 
+//if right one left
+
+while (right<=end)
+{
+    temp[index]=arr[+right];
+    index++;right++;
+}
+index=0;
+while (start<=end)
+{
+    arr[start]=temp[index];
+    start++;index++;
+}
+
+
+
+}
+
+
+
+
+
+void mergesort(int arr[],int start,int end){
+
+ if (start==end)
+ {
+    return;
+ }
+ int mid=start - (start-end)/2;
+
+ mergesort(arr,start,mid);
+ mergesort(arr,mid+1,end);
+
+ merge(arr,start,mid,end);
+ 
+
+
+
+
+}
+
+
+
+int main(){
+    
+int array[7] = {1, 5, 53, 64, 25, 16, 7};
+
+
+
+mergesort(array,0,6);
+    
+    for(int i = 0; i < 7; i++) {
+    cout << array[i] << " ";
+    }
+
+
+
+    return 0;
+}
