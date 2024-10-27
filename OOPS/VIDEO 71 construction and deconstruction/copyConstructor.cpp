d5878674a6b7b5e32f8e0e033a27b187df77573c
SAGAR-RAJ1 <ksanjeev49919+github@gmail.com>
2024-10-27 13:36:02 +0530

    copy custructor

---
+++ OOPS/VIDEO 71 construction and deconstruction/copyConstructor.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+class customer
+{
+
+    string name;
+    int account_number;
+    int balance;
+
+public:
+   
+    customer()
+    {
+        name = "sagar";
+        account_number = 123;
+        balance = 99999;
+    }
+
+    //inline constructor 
+    inline customer(string s,int a,int b):name(s),account_number(a),balance(b){
+        
+    }
+
+    //copy custructor
+customer(customer &b){
+    name=b.name;
+    account_number=b.account_number;
+    balance=b.balance;
+}
+    void display()
+    {
+        cout << name << " " << account_number << "  " << balance << endl;
+    }
+};
+
+int main()
+{
+    customer a1;
+    customer a2("rahul", 12, 34);
+    customer a3(a2);
+    customer a4(a1);
+    a1.display();
+    a2.display();
+    a3.display();
+    a4.display();
+    return 0;
+}
+
