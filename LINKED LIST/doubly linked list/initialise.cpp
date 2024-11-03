036ac6ca0c8fe40adf6fb74b73e2753df3327528
SAGAR-RAJ1 <ksanjeev49919+github@gmail.com>
2024-11-03 07:40:15 +0530

    initialise doubly linked list

---
+++ LINKED LIST/doubly linked list/initialise.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+class node {
+    public:
+    int data;
+    node * next;
+    node * prev;
+
+    node(int value){
+        data = value;
+        next=NULL;
+        prev=NULL;
+
+    }
+};
+void display(node * head){
+    node * trans=head;
+    while (trans)
+    {
+        cout<<trans->data<<"  ";
+        trans=trans->next;
+    }
+    
+}
+
+int main(){
+    int arr[5]={1,2,3,4,5};
+
+
+    node *head=NULL;
+    node *tail=NULL;
+
+for (int i = 0; i < 5; i++)
+{
+
+    if (head==NULL)
+    {
+        head = new node(arr[i]);
+        tail=head;
+    }
+    else{
+
+        node * temp=new node(arr[i]);
+        tail->next=temp;
+        temp->prev=tail;
+        tail=temp;
+    }
+}
+display(head);
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
