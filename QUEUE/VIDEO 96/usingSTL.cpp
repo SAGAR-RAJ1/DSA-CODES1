edd7a256c43668fa7c40ec15f56d150c17a4f06c
SAGAR-RAJ1 <ksanjeev49919+github@gmail.com>
2024-11-13 06:10:15 +0530

    queue using STL

---
+++ QUEUE/VIDEO 96/usingSTL.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+
+
+int main(){
+    queue<int>a;
+    a.push(4);
+   cout<< a.front();
+  a.pop();
+    return 0;
+}
