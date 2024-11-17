677d6a3a6587e509857ee7fd0d302eab497fdbfb
SAGAR-RAJ1 <ksanjeev49919+github@gmail.com>
2024-11-17 20:48:50 +0530

    code to implement binary tree is added

---
+++ DEQUEUE/VIDEO 100/implementusingSTL.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include<deque>
+using namespace std;
+
+
+
+int main(){
+    deque<int>q;
+      q.push_back(1);
+      q.push_back(2);
+      q.push_back(3);
+cout<<   q.size();
+    return 0;
+}
---
+++ TREES/VIDEO 102/implementationTREE.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *left;
+    node *right;
+
+    node(int x)
+    {
+        data = x;
+        left = right = NULL;
+    }
+};
+
+int main()
+{
+    queue<node *> q;
+    cout << "Enter the root element " << endl;
+    int x;
+    cin >> x;
+    node *root = new node(x);
+    q.push(root);
+    int left, right;
+    while (!q.empty())
+    {
+        node *temp = q.front();
+        q.pop();
+        cout << "enter left side value of "<<temp->data << endl;
+        cin >> left;
+
+        //[phle left create kro]
+        if (left != -1)
+        {
+            temp->left = new node(left);
+            q.push(temp->left);
+        }
+        cout << "enter right side value of "<<temp->data << endl;
+        cin >> right;
+        if (right != -1)
+        {
+            temp->right = new node(right);
+            q.push(temp->right);
+        }
+    }
+
+    return 0;
+}
